6f7fd461929fb2426ef55025dd437b0825aadc6b
Lorenzo Affetti <affox90@gmail.com>
2015-07-28 16:46:51 +0200

    First impl

--- app/src/main/java/it/playfellas/superapp/logic/common/slave/SlaveController.java
+++ app/src/main/java/it/playfellas/superapp/logic/common/slave/SlaveController.java
@@ -4,7 +4,11 @@ import android.util.Log;
 
 import com.squareup.otto.Subscribe;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import it.playfellas.superapp.events.ClickedTileEvent;
+import it.playfellas.superapp.events.EventFactory;
 import it.playfellas.superapp.events.GameChangeEvent;
 import it.playfellas.superapp.logic.common.tiles.Tile;
 import it.playfellas.superapp.network.TenBus;
@@ -17,7 +21,15 @@ public abstract class SlaveController {
     private static final String TAG = SlaveController.class.getSimpleName();
     @Setter
     protected TileDispenser dispenser; // apply strategy here
-    private float currentRtt; // TODO: set timer that increments periodically rtt
+    //TODO: from config, based on the difficultyLevel
+    private static final float defaultRtt = 5;
+    private static final int rttPeriod = 10;
+    private static final float minRtt = 3;
+    private static final int noDifficultySteps = 5;
+
+    private static final float decreaseFraction = Math.abs(defaultRtt - minRtt) / noDifficultySteps;
+    private float currentRtt;
+    private Timer rttDownCounter;
 
     public SlaveController() {
         super();
@@ -27,10 +39,43 @@ public abstract class SlaveController {
     // override to implement the logic of the game
     public abstract boolean isTileRight(Tile t);
 
+    public void start() {
+        rttDownCounter = new Timer(true);
+        resetRtt();
+        // scheduling `decreaseRtt` after `rttPeriod`
+        // with period `rttPeriod`.
+        rttDownCounter.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                decreaseRtt();
+            }
+        }, rttPeriod, rttPeriod);
+    }
+
+    public void stop() {
+        rttDownCounter.cancel();
+        rttDownCounter.purge();
+    }
+
+    private void resetRtt() {
+        currentRtt = defaultRtt;
+    }
+
+    private void decreaseRtt() {
+        if (currentRtt <= minRtt) {
+            rttDownCounter.cancel();
+            return;
+        }
+        
+        currentRtt -= decreaseFraction;
+        TenBus.get().post(EventFactory.rttUpdate(currentRtt));
+        dispenser.setRtt(currentRtt);
+    }
+
     @Subscribe
     public void onGameChange(GameChangeEvent e) {
         Class<TileDispenser> dispenserClass = e.getDispenserClass();
-        TileDispenser newDispenser = null;
+        TileDispenser newDispenser;
         try {
             // TileDispenser constructor should be empty!
             newDispenser = dispenserClass.newInstance();
@@ -49,12 +94,9 @@ public abstract class SlaveController {
     @Subscribe
     public void onTileClicked(ClickedTileEvent e) {
         Tile t = e.getTile();
-        if (isTileRight(t)) {
-            Log.d(TAG, "Clicked on right tile");
-            //TODO: post NetEvent for right tile
-        } else {
-            Log.d(TAG, "Clicked on wrong tile");
-            //TODO: post Netevent for wrong tile
-        }
+        boolean rw = isTileRight(t);
+        String rwWord = rw ? "Correct" : "Incorrect";
+        Log.d(TAG, rwWord + " answer given");
+        TenBus.get().post(EventFactory.rw(rw));
     }
 }
