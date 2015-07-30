edf8e192fd8df959e2c36046bd84b8d60d27874d
Lorenzo Affetti <affox90@gmail.com>
2015-07-30 14:57:09 +0200

    abstract config first impl

---
+++ app/src/main/java/it/playfellas/superapp/logic/common/Config.java
@@ -0,0 +1,46 @@
+package it.playfellas.superapp.logic.common;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by affo on 30/07/15.
+ */
+public abstract class Config {
+    private static final int decreaseSteps = 5;
+    @Getter
+    private static final int rttUpdatePeriod = 10;
+    private static final float baseMaxRtt = 5;
+    private static final float baseMinRtt = 2;
+
+    @Getter
+    @Setter
+    private int difficultyLevel;
+    @Getter
+    @Setter
+    private int tileDensity;
+    @Getter
+    @Setter
+    private int maxScore;
+    @Getter
+    @Setter
+    private int noStages;
+    @Getter
+    @Setter
+    private int ruleChange;
+    @Getter
+    @Setter
+    private boolean speedUp;
+
+    public float getDefaultRtt() {
+        return baseMaxRtt - (float) difficultyLevel / 2;
+    }
+
+    public float getMinRtt() {
+        return baseMinRtt - (float) difficultyLevel / 4;
+    }
+
+    public float getRttDecreaseDelta() {
+        return Math.abs(getDefaultRtt() - getMinRtt()) / decreaseSteps;
+    }
+}
