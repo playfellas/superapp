b040fee09ab7bff573dcdef98c06dffb5f70c5b7
Stefano Cappa <stefano.cappa@mail.polimi.it>
2015-08-03 13:38:11 +0200

    Added masterController but not really used

--- app/src/main/java/it/playfellas/superapp/activities/master/game1/Game1FragmentPresenter.java
+++ app/src/main/java/it/playfellas/superapp/activities/master/game1/Game1FragmentPresenter.java
@@ -1,11 +1,19 @@
 package it.playfellas.superapp.activities.master.game1;
 
+import it.playfellas.superapp.logic.Config1;
+import it.playfellas.superapp.logic.master.Master1Controller;
+
 public class Game1FragmentPresenter {
 
-  private Game1Fragment fragment;
+    private Game1Fragment fragment;
 
-  public void onTakeView(Game1Fragment fragment) {
-    this.fragment = fragment;
-  }
+    private Master1Controller master1 = new Master1Controller(new Config1());
+
+
+    public void onTakeView(Game1Fragment fragment) {
+        this.fragment = fragment;
+
+//        master1.beginStage();
+    }
 
 }
