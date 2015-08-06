a87389eab9767fed4f79a996075c6f0538c69c76
Lorenzo Affetti <affox90@gmail.com>
2015-08-06 14:27:38 +0200

    Fix, join with spaces

--- app/src/main/java/it/playfellas/superapp/logic/db/query/BinaryOperator.java
+++ app/src/main/java/it/playfellas/superapp/logic/db/query/BinaryOperator.java
@@ -4,10 +4,10 @@ package it.playfellas.superapp.logic.db.query;
  * Created by affo on 06/08/15.
  */
 public enum BinaryOperator {
-    DIFFERENT("<>"),
-    EQUALS("="),
-    AND("AND"),
-    OR("OR");
+    DIFFERENT(" <> "),
+    EQUALS(" = "),
+    AND(" AND "),
+    OR(" OR ");
 
     private String op;
 
