da3bb5af09de8b04ff241ba0ea9a797b5c4e0eab
Lorenzo Affetti <affox90@gmail.com>
2015-08-06 13:31:18 +0200

    randutils, finally

---
+++ app/src/main/java/it/playfellas/superapp/logic/RandomUtils.java
@@ -0,0 +1,36 @@
+package it.playfellas.superapp.logic;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by affo on 06/08/15.
+ */
+public class RandomUtils {
+
+    public static <T> List<T> choice(Collection<T> coll, int n) {
+        int size = n > coll.size() ? coll.size() : n;
+        T[] a = (T[]) coll.toArray();
+
+        List<Integer> indexes = new ArrayList<>();
+        for (int i = 0; i < coll.size(); i++) {
+            indexes.add(i);
+        }
+
+        Collections.shuffle(indexes);
+        List<Integer> rndIndexes = indexes.subList(0, size);
+
+        List<T> ret = new ArrayList<>();
+        for (Integer i : rndIndexes) {
+            ret.add(a[i]);
+        }
+
+        return ret;
+    }
+
+    public static <T> T choice(Collection<T> coll) {
+        return choice(coll, 1).get(0);
+    }
+}
