a02c637a01d3c4cd5b8724b9ffa1337c4df42b34
Lorenzo Affetti <affox90@gmail.com>
2015-09-01 17:01:13 +0200

    better TileType for images

--- app/src/main/java/it/playfellas/superapp/logic/db/DbFiller.java
+++ app/src/main/java/it/playfellas/superapp/logic/db/DbFiller.java
@@ -41,29 +41,29 @@ public class DbFiller {
 
     private List<Tile> createTiles() {
         List<Tile> tiles = new ArrayList<>();
-        //String url, TileColor color, TileDirection direction, TileShape shape, TileType type, int size)
-        tiles.add(new Tile("anguria", TileColor.GREEN, false, TileShape.CIRCLE, TileType.ABSTRACT));
-        tiles.add(new Tile("auto", TileColor.RED, true, TileShape.NONE, TileType.ABSTRACT));
-        tiles.add(new Tile("auto_intruso", TileColor.YELLOW, true, TileShape.NONE, TileType.ABSTRACT));
-        tiles.add(new Tile("bottone", TileColor.BLUE, false, TileShape.CIRCLE, TileType.ABSTRACT));
-        tiles.add(new Tile("dado", TileColor.WHITE, false, TileShape.CUBE, TileType.ABSTRACT));
-        tiles.add(new Tile("fish", TileColor.RED, true, TileShape.NONE, TileType.ABSTRACT));
-        tiles.add(new Tile("fish_intruso", TileColor.YELLOW, true, TileShape.NONE, TileType.ABSTRACT));
-        tiles.add(new Tile("leone", TileColor.NONE, false, TileShape.NONE, TileType.ABSTRACT));
+        //String drawable_id, TileColor color, boolean directionable, TileShape shape, TileType type
+        tiles.add(new Tile("anguria", TileColor.GREEN, false, TileShape.CIRCLE, TileType.CONCRETE));
+        tiles.add(new Tile("auto", TileColor.RED, true, TileShape.NONE, TileType.CONCRETE));
+        tiles.add(new Tile("auto_intruso", TileColor.YELLOW, true, TileShape.NONE, TileType.CONCRETE));
+        tiles.add(new Tile("bottone", TileColor.BLUE, false, TileShape.CIRCLE, TileType.CONCRETE));
+        tiles.add(new Tile("dado", TileColor.WHITE, false, TileShape.CUBE, TileType.CONCRETE));
+        tiles.add(new Tile("fish", TileColor.RED, true, TileShape.NONE, TileType.CONCRETE));
+        tiles.add(new Tile("fish_intruso", TileColor.YELLOW, true, TileShape.NONE, TileType.CONCRETE));
+        tiles.add(new Tile("leone", TileColor.NONE, false, TileShape.NONE, TileType.CONCRETE));
         tiles.add(new Tile("pallino_intruso", TileColor.LIGHT_BLUE, false, TileShape.CIRCLE, TileType.ABSTRACT));
         tiles.add(new Tile("quadrato", TileColor.GREEN, false, TileShape.SQUARE, TileType.ABSTRACT));
-        tiles.add(new Tile("quadrifoglio", TileColor.GREEN, false, TileShape.NONE, TileType.ABSTRACT));
+        tiles.add(new Tile("quadrifoglio", TileColor.GREEN, false, TileShape.NONE, TileType.CONCRETE));
         tiles.add(new Tile("stella", TileColor.YELLOW, false, TileShape.NONE, TileType.ABSTRACT));
-        tiles.add(new Tile("tartaruga", TileColor.GREEN, true, TileShape.CIRCLE, TileType.ABSTRACT));
+        tiles.add(new Tile("tartaruga", TileColor.GREEN, true, TileShape.CIRCLE, TileType.CONCRETE));
         tiles.add(new Tile("tasto_avanti", TileColor.ORANGE, true, TileShape.CIRCLE, TileType.ABSTRACT));
         tiles.add(new Tile("tasto_esci", TileColor.ORANGE, true, TileShape.CIRCLE, TileType.ABSTRACT));
         tiles.add(new Tile("tasto_intruso_dinamico", TileColor.ORANGE, false, TileShape.SQUARE, TileType.ABSTRACT));
         tiles.add(new Tile("tasto_intruso_statico", TileColor.ORANGE, false, TileShape.SQUARE, TileType.ABSTRACT));
         tiles.add(new Tile("tasto_rigioca", TileColor.ORANGE, false, TileShape.CIRCLE, TileType.ABSTRACT));
-        tiles.add(new Tile("tigre", TileColor.NONE, false, TileShape.NONE, TileType.ABSTRACT));
+        tiles.add(new Tile("tigre", TileColor.NONE, false, TileShape.NONE, TileType.CONCRETE));
         tiles.add(new Tile("triangolo_1", TileColor.ORANGE, true, TileShape.TRIANGLE, TileType.ABSTRACT));
         tiles.add(new Tile("triangolo_2", TileColor.RED, true, TileShape.TRIANGLE, TileType.ABSTRACT));
-        tiles.add(new Tile("trifoglio", TileColor.GREEN, false, TileShape.NONE, TileType.ABSTRACT));
+        tiles.add(new Tile("trifoglio", TileColor.GREEN, false, TileShape.NONE, TileType.CONCRETE));
         return tiles;
     }
 }
