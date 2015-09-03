d5570718a382782fb77808a15510d1bef58a888f
Stefano Cappa <stefano.cappa@mail.polimi.it>
2015-09-03 23:01:02 +0200

    Added Slave3Utils

---
+++ app/src/main/java/it/playfellas/superapp/ui/slave/game3/Slave3Utils.java
@@ -0,0 +1,54 @@
+package it.playfellas.superapp.ui.slave.game3;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.widget.ImageView;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import it.playfellas.superapp.InternalConfig;
+import it.playfellas.superapp.R;
+import it.playfellas.superapp.logic.tiles.Tile;
+import it.playfellas.superapp.logic.tiles.TileSize;
+import it.playfellas.superapp.ui.BitmapUtils;
+
+/**
+ * Created by Stefano Cappa on 03/09/15.
+ */
+public class Slave3Utils {
+
+
+    public static void updateCompleteTower(Tile[] tiles, ImageView[] completeImageView, Resources resources) {
+        for (int i = 0; i < tiles.length && tiles[i] != null; i++) {
+            completeImageView[i].setImageBitmap(BitmapUtils.scaleInsideWithFrame(getBitmapFromTile(tiles[i], resources), getTileSizes()[i].getMultiplier(), Color.TRANSPARENT));
+        }
+    }
+
+    public static TileSize[] getTileSizes() {
+        TileSize[] sizes = TileSize.values();
+        ArrayUtils.reverse(sizes);
+        return sizes;
+    }
+
+    public static void updateSlotsTower(Tile[] tiles, ImageView[] slotsImageView, Resources resources) {
+        for (int i = 0; i < tiles.length; i++) {
+            if (tiles[i] != null) {
+                slotsImageView[i].setImageBitmap(BitmapUtils.scaleInsideWithFrame(getBitmapFromTile(tiles[i], resources), getTileSizes()[i].getMultiplier(), Color.TRANSPARENT));
+            } else {
+                //if null add a transparent image
+                slotsImageView[i].setImageBitmap(getBitmapFromId(R.drawable.trasparente, resources));
+            }
+        }
+    }
+
+    public static Bitmap getBitmapFromTile(Tile t, Resources resources) {
+        int resId = resources.getIdentifier(t.getName(), InternalConfig.DRAWABLE_RESOURCE, InternalConfig.PACKAGE_NAME);
+        return getBitmapFromId(resId, resources);
+    }
+
+    public static Bitmap getBitmapFromId(int resId, Resources resources) {
+        return BitmapFactory.decodeResource(resources, resId);
+    }
+}
