a1c82300d54ed1a7f562351683dfe4dc11a58b59
Stefano Cappa <stefano.cappa@mail.polimi.it>
2015-09-06 01:17:10 +0200

    Added Fabric. I removed the file app/fabric.properties because must be secret

--- app/src/main/java/it/playfellas/superapp/SuperAppApplication.java
+++ app/src/main/java/it/playfellas/superapp/SuperAppApplication.java
@@ -3,6 +3,8 @@ package it.playfellas.superapp;
 import android.app.Application;
 
 import com.firebase.client.Firebase;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by affo on 04/09/15.
@@ -11,6 +13,7 @@ public class SuperAppApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         Firebase.setAndroidContext(this);
     }
 }
