f18820783fce029193760676b9a32e23dde89f7e
Stefano Cappa <stefano.cappa@mail.polimi.it>
2015-09-05 02:03:06 +0200

    Added a new DialogFragment with a countdown inside on master, displayed at every endstageevent

---
+++ app/src/main/java/it/playfellas/superapp/ui/master/MasterTimerDialogFragment.java
@@ -0,0 +1,83 @@
+package it.playfellas.superapp.ui.master;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import it.playfellas.superapp.R;
+
+/**
+ * Created by Stefano Cappa on 05/09/15.
+ */
+public class MasterTimerDialogFragment extends DialogFragment {
+
+    @Bind(R.id.countDownTextView)
+    TextView countDownTextView;
+
+    private static String titleString;
+
+    public interface DialogTimerListener {
+        void onCountdownFinished();
+    }
+
+    /**
+     * Method to obtain a new Fragment's instance.
+     *
+     * @return This Fragment instance.
+     */
+    public static MasterTimerDialogFragment newInstance(String title) {
+        titleString = title;
+        return new MasterTimerDialogFragment();
+    }
+
+    /**
+     * Default Fragment constructor.
+     */
+    public MasterTimerDialogFragment() {
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        if (getDialog() != null && getRetainInstance()) {
+            getDialog().setOnDismissListener(null);
+        }
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.master_timer_endstage_dialog, container, false);
+
+        //ButterKnife bind version for fragments
+        ButterKnife.bind(this, v);
+
+        getDialog().setTitle(titleString);
+        countDownTextView.setText("5");
+
+        CountDownTimer countDownTimer = new CountDownTimer(5000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                countDownTextView.setText((millisUntilFinished / 1000) + "");
+            }
+
+            public void onFinish() {
+                countDownTextView.setText("Avvio in corso...");
+                //Go back to che caller fragment.
+                ((DialogTimerListener) getTargetFragment()).onCountdownFinished();
+                //and dismiss
+                dismiss();
+            }
+        };
+        countDownTimer.start();
+
+        return v;
+    }
+}
