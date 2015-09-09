0940279f9974867b9b07348342f54c36293da1e1
Lorenzo Affetti <affox90@gmail.com>
2015-09-09 14:33:13 +0200

    on app init, check if DB is empty or not

--- app/src/main/java/it/playfellas/superapp/SuperAppApplication.java
+++ app/src/main/java/it/playfellas/superapp/SuperAppApplication.java
@@ -1,6 +1,8 @@
 package it.playfellas.superapp;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
@@ -16,6 +18,7 @@ import it.playfellas.superapp.logic.db.DbFiller;
  */
 public class SuperAppApplication extends Application {
     private static final String TAG = SuperAppApplication.class.getSimpleName();
+    private static final String DBFILL_PREF = "dbfill";
 
     @Override
     public void onCreate() {
@@ -26,13 +29,17 @@ public class SuperAppApplication extends Application {
 
         // Firebase DB
         Firebase.setAndroidContext(this);
-        
+
         // Fill internal DB
-        try {
-            DbAccess db = new DbAccess(this);
-            (new DbFiller(db)).fill();
-        } catch (DbException e) {
-            Log.e(TAG, "DbException", e);
+        SharedPreferences pref = getSharedPreferences(getString(R.string.preference_key_app), Context.MODE_PRIVATE);
+        if (!pref.getBoolean(DBFILL_PREF, false)) {
+            try {
+                DbAccess db = new DbAccess(this);
+                (new DbFiller(db)).fill();
+                pref.edit().putBoolean(DBFILL_PREF, true).apply();
+            } catch (DbException e) {
+                Log.e(TAG, "DbException", e);
+            }
         }
     }
 }
