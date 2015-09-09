1bb83edb1c7986eb33217951961cf8955046c6d4
Lorenzo Affetti <affox90@gmail.com>
2015-09-09 15:00:48 +0200

    Fix in game3 basetiles selection logic

--- app/src/main/java/it/playfellas/superapp/logic/master/game23/Master3Controller.java
+++ app/src/main/java/it/playfellas/superapp/logic/master/game23/Master3Controller.java
@@ -4,27 +4,26 @@ import android.util.Log;
 
 import com.squareup.otto.Subscribe;
 
-import org.apache.commons.lang3.ArrayUtils;
-
 import java.util.Arrays;
 
 import it.playfellas.superapp.InternalConfig;
+import it.playfellas.superapp.RandomUtils;
 import it.playfellas.superapp.events.EventFactory;
 import it.playfellas.superapp.events.game.PopEvent;
 import it.playfellas.superapp.events.game.PushEvent;
 import it.playfellas.superapp.events.game.StartGameEvent;
 import it.playfellas.superapp.logic.Config3;
-import it.playfellas.superapp.RandomUtils;
 import it.playfellas.superapp.logic.db.TileSelector;
 import it.playfellas.superapp.logic.db.query.BinaryOperator;
+import it.playfellas.superapp.logic.db.query.Color;
 import it.playfellas.superapp.logic.db.query.Conjunction;
 import it.playfellas.superapp.logic.db.query.Shape;
 import it.playfellas.superapp.logic.db.query.Type;
+import it.playfellas.superapp.network.TenBus;
 import it.playfellas.superapp.tiles.Tile;
 import it.playfellas.superapp.tiles.TileColor;
 import it.playfellas.superapp.tiles.TileShape;
 import it.playfellas.superapp.tiles.TileType;
-import it.playfellas.superapp.network.TenBus;
 
 /**
  * Created by affo on 02/09/15.
@@ -47,25 +46,18 @@ public class Master3Controller extends Master23Controller {
     @Override
     protected Tile[] newBaseTiles() {
         final int size = InternalConfig.NO_FIXED_TILES;
-        // remove NONE...
+
         TileColor[] colors = TileColor.values();
-        TileShape[] shapes = TileShape.values();
-        int noneIndex;
-
-        noneIndex = ArrayUtils.indexOf(colors, TileColor.NONE);
-        colors = ArrayUtils.remove(colors, noneIndex);
-
-        noneIndex = ArrayUtils.indexOf(shapes, TileShape.NONE);
-        shapes = ArrayUtils.remove(shapes, noneIndex);
 
         colors = RandomUtils.choice(colors, size).toArray(new TileColor[size]);
-        shapes = RandomUtils.choice(shapes, size).toArray(new TileShape[size]);
         Tile[] tiles = new Tile[InternalConfig.NO_FIXED_TILES];
 
         for (int i = 0; i < size; i++) {
             tiles[i] = ts.random(1, new Conjunction(
-                    new Shape(BinaryOperator.EQUALS, shapes[i]),
-//                    new Color(BinaryOperator.EQUALS, colors[i]),
+                    // We want shape to be NONE because of db reasons
+                    // emerged in last specifications.
+                    new Shape(BinaryOperator.EQUALS, TileShape.NONE),
+                    new Color(BinaryOperator.EQUALS, colors[i]),
                     new Type(BinaryOperator.EQUALS, TileType.ABSTRACT)
             )).get(0);
         }
