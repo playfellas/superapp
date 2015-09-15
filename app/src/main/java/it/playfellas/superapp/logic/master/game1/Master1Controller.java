ce695921cfed8093361c8f79d158ee7a2b2b6a38
Lorenzo Affetti <affox90@gmail.com>
2015-09-15 10:27:27 +0200

    Posting UI toggle game mode event after the associated NetEvent

--- app/src/main/java/it/playfellas/superapp/logic/master/game1/Master1Controller.java
+++ app/src/main/java/it/playfellas/superapp/logic/master/game1/Master1Controller.java
@@ -1,7 +1,6 @@
 package it.playfellas.superapp.logic.master.game1;
 
 import it.playfellas.superapp.events.EventFactory;
-import it.playfellas.superapp.events.game.StartGameEvent;
 import it.playfellas.superapp.logic.Config1;
 import it.playfellas.superapp.logic.master.MasterController;
 import it.playfellas.superapp.network.TenBus;
@@ -12,22 +11,23 @@ import it.playfellas.superapp.network.TenBus;
 public abstract class Master1Controller extends MasterController {
     private Config1 conf;
 
-    public Master1Controller(Config1 conf){
+    public Master1Controller(Config1 conf) {
         super(conf);
         this.conf = conf;
     }
 
     @Override
     protected void onAnswer(boolean rw) {
-        if (rw){
+        if (rw) {
             incrementScore();
         } else {
             resetScore();
         }
-        
+
         int score = getScore();
         if (score != 0 && score % conf.getRuleChange() == 0) {
             TenBus.get().post(EventFactory.gameChange());
+            TenBus.get().post(EventFactory.uiToggleGameMode());
         }
     }
 }
