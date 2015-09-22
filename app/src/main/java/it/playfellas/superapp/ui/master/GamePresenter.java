47952bc51227e83b4d4fa71c1d20faad897053cd
Stefano Cappa <stefano.cappa@mail.polimi.it>
2015-09-22 11:05:36 +0200

    Fixed sounds

--- app/src/main/java/it/playfellas/superapp/ui/master/GamePresenter.java
+++ app/src/main/java/it/playfellas/superapp/ui/master/GamePresenter.java
@@ -99,7 +99,6 @@ public abstract class GamePresenter {
     protected void init() {
         this.master = newMasterController();
         fragment.initSounds();
-        fragment.playMusic();
     }
 
     protected abstract MasterController newMasterController();
